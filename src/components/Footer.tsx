ecea4338aa6ae2e51c4c86b10809405e020f7f90
juanf <yioyiomain@gmail.com>
2024-08-26 16:33:15 -0500

    Footer submain2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,18 @@ import '../styles/Footer.css'
 export default function Footer() {
     return(
         <footer className='footer'>
-        This is the footer
+            <ul className='menu'>
+                <li className='menu-elem'>
+                    <a href="#" className='menu-icon'>Inicio</a>
+                </li>
+                <li className='menu-elem'>
+                    <a href="#" className='menu-icon'>Curso</a>
+                </li>
+                <li className='menu-elem'>
+                    <a href="#" className='menu-icon'>Contacto</a>
+                </li>
+            </ul>
+            <p className='text'>2024 | Juan Francisco Osorio Aguirre</p>
         </footer>
     )
 }
